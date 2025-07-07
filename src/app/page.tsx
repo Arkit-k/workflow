55a8a17e0034e7263e8ecbfbe61b5953c0a37165
arkit_k <arkitkarmokar007@gmail.com>
2025-07-07 19:27:19 +0530

    first-commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import { Button } from "~/components/ui/button";
+
+
+
+export default async function Home() {
+  return (
+    <div><h1>
+      <Button>hi</Button></h1></div>
+  );
+}

fbdf00a8731fef823985635013108655a620f835
arkit_k <arkitkarmokar007@gmail.com>
2025-07-13 13:25:23 +0530

    damncommited

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { useAuth } from '@clerk/nextjs'
+import { ExternalLink, Github } from 'lucide-react'
 import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 import React, { useEffect } from 'react'
 import useProject from '~/hooks/use-project'
 
@@ -25,9 +27,40 @@ const SyncUser = () => {
   }
 
   return (
-    <div>
-      <h1>{project?.name || 'No project selected'}</h1>
+   <div>
+    <div className='flex item-center justify-between flex-wrap gap-y-4'>
+    <div className='w-fit rounded-md bg-primary px-4 py-3'>
+      <div className='flex item-center'>
+      <Github className='size-5 text-white'/>
+      <div className='ml-2'>
+        <p className='text-sm font-medium text-white'>{""}{project?.name}
+          <Link href={project?.githubUrl ?? " "} className='inline-flex items-center text-white/80 hover:underline'>
+          {project?.githubUrl}
+          <ExternalLink className='ml-1 size-4' />
+          </Link>
+        </p>
+      </div>
     </div>
+    </div>
+    <div className='flex items-center gap-4'>
+      TeamMembers
+      InviteButton
+      ArchiveButton
+    </div>
+    <div className='flex items-center gap-4'>
+
+    </div>
+    </div>
+    <div className='mt-4'>
+    <div className='grid grid-cols-1 gap-4 sm:grid-cols-5'>
+      AskQustionCard
+      MeetingCard
+    </div>
+    </div>
+    <div className='mt-8'>
+      connetlog
+    </div>
+  </div>
   )
 }
 
